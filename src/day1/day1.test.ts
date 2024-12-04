98f26b30a5080b0ab3fcfade2cfdd78275330e42
Patrick Pfeiler <pfeiler.patrick@live.de>
2024-12-04 20:04:20 +0100

    feat: solved day1, part 2

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { solve1 } from './day1';
+import { solve1, solve2 } from './day1';
 import * as fs from 'fs';
 
 describe("solve day 1", () => {
@@ -21,5 +21,25 @@ describe("solve day 1", () => {
         const fileInput = fs.readFileSync("./src/day1/input1.1.txt","utf8");
         const result = solve1(fileInput.split("\n"));
         expect(result).toBe(2756096);
+    });
+
+    it("solve day 1.2 example", () => {
+        const input =
+            [
+                "3   4",
+                "4   3",
+                "2   5",
+                "1   3",
+                "3   9",
+                "3   3"
+            ];
+        const result = solve2(input);
+        expect(result).toBe(31);
+    });
+
+    it("solve day 1.2", () => {
+        const fileInput = fs.readFileSync("./src/day1/input1.1.txt","utf8");
+        const result = solve2(fileInput.split("\n"));
+        expect(result).toBe(23117829);
     });
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -22,3 +22,27 @@ export function solve1(input: string[]): number {
 
     return totalDistance;
 }
+
+export function solve2(input: string[]): number {
+    const list1: number[] = []
+    const list2: Map<number, number> = new Map();
+
+    input.forEach((line: string) => {
+        const parts = line.split("   ");
+        list1.push(parseInt(parts[0]))
+        const rightValue = parseInt(parts[1]);
+        
+        if (list2.has(rightValue)) {
+            list2.set(rightValue, list2.get(rightValue)!+1)
+        } else {
+            list2.set(rightValue, 1);
+        }
+    })
+
+    let similarityScore = 0;
+    list1.forEach((locationId1) => {
+        similarityScore += locationId1 * (list2.get(locationId1) ?? 0);
+    });
+
+    return similarityScore;
+}
